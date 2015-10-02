8da717a922ff701956afe986b72fc13ebed41ceb
fritz-smh <fritz.smh@gmail.com>
2015-10-02 22:45:01 +0200

    small improvments, icon, doc, admin page

---
+++ admin/__init__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+### common imports
+from flask import Blueprint, abort
+from domogik.common.utils import get_packages_directory
+from domogik.admin.application import render_template
+from domogik.admin.views.clients import get_client_detail
+from jinja2 import TemplateNotFound
+
+### package specific imports
+import subprocess
+
+
+
+### package specific functions
+def list_bt_devices():
+    bt = subprocess.Popen(["hcitool", "scan"], stdout=subprocess.PIPE)
+    output = bt.communicate()[0]
+    if isinstance(bt, str):
+        output = unicode(output, 'utf-8')
+    return output
+
+
+
+
+### common tasks
+package = "plugin_bluez"
+template_dir = "{0}/{1}/admin/templates".format(get_packages_directory(), package)
+static_dir = "{0}/{1}/admin/static".format(get_packages_directory(), package)
+
+plugin_bluez_adm = Blueprint(package, __name__,
+                        template_folder = template_dir,
+                        static_folder = static_dir)
+
+@plugin_bluez_adm.route('/<client_id>')
+def index(client_id):
+    detail = get_client_detail(client_id)
+    try:
+        return render_template('plugin_bluez.html',
+            clientid = client_id,
+            client_detail = detail,
+            mactive="clients",
+            active = 'advanced',
+            bt = list_bt_devices())
+
+    except TemplateNotFound:
+        abort(404)
+
--- docs/conf.py
+++ docs/conf.py
@@ -1,24 +1,49 @@
+# -*- coding: utf-8 -*-
+#
+
 import sys
 import os
 
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
     'sphinx.ext.todo',
 ]
 
+# The suffix of source filenames.
 source_suffix = '.txt'
 
+# The master toctree document.
 master_doc = 'index'
 
-### part to update ###################################
+# General information about the project.
 project = u'domogik-plugin-bluez'
-copyright = u'2015, Cereal'
-version = '0.4'
-release = version
-######################################################
+copyright = u'2015, Cereal, S. Gallet'
 
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+version = '1.0'
+# The full version, including alpha/beta/rc tags.
+release = '1.0'
+
+# The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
-   
+
+
+# -- Options for HTML output ----------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
 html_theme = 'default'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-htmlhelp_basename = project
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'domogik-plugin-bluez'
 
